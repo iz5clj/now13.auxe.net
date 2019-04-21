f9b583a53164fa28626ee9ed5bf2a1af319200f2
Michel <ms@auxe.net>
2019-04-21 17:16:33 +0200

    added bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var count = require('gulp-count');
+var print = require('gulp-print').default;
+
+gulp.task('copy_bootstrap_sass', function() {
+    gulp.src('node_modules/bootstrap/scss/**/*')
+    .pipe(gulp.dest('assets/sass/bootstrap/'))
+    .pipe(print())
+    .pipe(count('### files processed.'));
+});
+
+gulp.task('copy_js', function() {
+    gulp.src([
+        'node_modules/popper.js/dist/umd/popper.js',
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/bootstrap/dist/js/bootstrap.js'
+    ])
+    .pipe(gulp.dest('assets/js/'))
+    .pipe(print())
+    .pipe(count('## files have been copied'));
+});
+
+gulp.task('minify', () => {
+    return gulp.src(['public/**/*.html', '!public/assets/**/*'])
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('public'))
+        .pipe(print())
+        .pipe(count('## files processed.'));
+});
+
+gulp.task('default', ['copy_bootstrap_sass', 'copy_js'], function () {
+    console.log('Done!');
+});
